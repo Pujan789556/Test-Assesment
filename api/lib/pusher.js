ed60087fd93cdb3dadaacfb33f4ad34f0e66711f
Pujan Poudyal <pujan789556@gmail.com>
2025-12-18 15:42:39 +0545

    Handle pusher configuration not avaibale. Disable if enviroment variable is not provided.

--- api/lib/pusher.js
+++ api/lib/pusher.js
@@ -2,20 +2,34 @@ const Pusher = require('pusher');
 
 class PusherService {
   constructor() {
+    const { PUSHER_APP_ID, PUSHER_KEY, PUSHER_SECRET, PUSHER_CLUSTER } = process.env;
+
+    this.enabled = !!PUSHER_APP_ID && !!PUSHER_KEY && !!PUSHER_SECRET && !!PUSHER_CLUSTER;
+
+    if (!this.enabled) {
+      console.warn('Pusher is disabled!. Missing enviroment variable!');
+      this.pusher = null;
+      return;
+    }
+
     this.pusher = new Pusher({
-      appId: process.env.PUSHER_APP_ID,
-      key: process.env.PUSHER_KEY,
-      secret: process.env.PUSHER_SECRET,
-      cluster: process.env.PUSHER_CLUSTER,
+      appId: PUSHER_APP_ID,
+      key: PUSHER_KEY,
+      secret: PUSHER_SECRET,
+      cluster: PUSHER_CLUSTER,
       useTLS: false,
     });
+
+    console.log('Pusher initialized!');
   }
 
   createMessage(message) {
+    if (!this.enabled) return;
     return this.pusher.trigger('message-channel', 'create-message', message);
   }
 
   deleteMessage(messageId) {
+    if (!this.enabled) return;
     return this.pusher.trigger('message-channel', 'delete-message', messageId);
   }
 }
